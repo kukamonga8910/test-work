50e6edfaaed032c1e3b0a2998ecde6b0a7d6a8a5
kukamonga8910 <kukamonga8910@gmail.com>
2024-11-29 16:42:14 +0300

    dz2

---
+++ dz2/failed_name.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function failed_name {
+    local dir="$1"
+
+    for item in "$dir"/*; do
+        if [[ -d "$item" ]]; then
+                failed_name "$item"
+        elif [[ -f "$item" ]]; then
+            filename=$(basename "$item")
+            if [[ "$filename" =~ [^a-zA-Z0-9_.-] ]]; then
+                # rm "$item"
+		echo "DELETE: $item"
+            fi
+        fi
+    done
+}
+
+# Пример использования
+#dir="/home/are/dz2"
+failed_name "."
+
+
